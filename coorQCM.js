56f7380492bec028bbd589958b4c5ec9ae995198
jaspir <comercialjerome@bbox.fr>
2024-09-28 18:28:50 +0200

    first commit

--- coorQCM.js
+++ coorQCM.js
@@ -1,11 +1,11 @@
-  document.getElementById("coorQCSGBD").innerHTML = " / Correction";
-let date1 = new Date(2024, 9, 2, 16,15);
-let date = new Date();
+
+let date1 = new Date(2024, 9, 2, 16,15); //date et heure limite
+let date = new Date();//date courante
 let message;
 if (date > date1) {
   message =" / "+
     '<a href="https://genumsi.inria.fr/qcm-corrige.php?cle=MTk0MzsxNjcwOzE3NzU7MTc3NDsxNDkxOzIwOTk7MTkyNjsxMzg5OzE3Nzk7MTkyODsxOTI3OzEzOTA7MTg5NDsyNTsxMzc4OzEzODQ7MjEwMDsyMTA0OzE5Mzk7MTg5NQ==">Correction</a>';
-  document.getElementById("coorQCSGBD").innerHTML = message;
+  document.getElementById("coorQCMSGBD").innerHTML = message;
 } else {
-  document.getElementById("coorQCSGBD").innerHTML = " / Correction";
+  document.getElementById("coorQCMSGBD").innerHTML = " / Correction";
 }
